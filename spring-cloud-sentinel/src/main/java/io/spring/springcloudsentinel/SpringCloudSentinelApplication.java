318f277a698c846a2bb4b65c1ccf0d69ba6c99dd
xiaokexiang <lijie@beyondcent.com>
2020-10-20 14:50:59 +0800

    spring-cloud 结构优化

---
+++ spring-cloud-sentinel/src/main/java/io/spring/springcloudsentinel/SpringCloudSentinelApplication.java
@@ -0,0 +1,13 @@
+package io.spring.springcloudsentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudSentinelApplication.class, args);
+    }
+
+}
