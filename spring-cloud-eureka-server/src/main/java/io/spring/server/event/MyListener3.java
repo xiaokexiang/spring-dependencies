af0f053a60094d6403d6fd711311b635ebb6029e
xiaokexiang <xxiaokexiang@gmail.com>
2020-11-10 11:13:17 +0800

    spring-event父子容器问题

--- spring-cloud-eureka-server/src/main/java/io/spring/server/event/MyListener3.java
+++ spring-cloud-eureka-server/src/main/java/io/spring/server/event/MyListener3.java
@@ -2,14 +2,26 @@ package io.spring.server.event;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationListener;
 
-public class MyListener3 implements ApplicationListener<MyEvent> {
+public class MyListener3 implements ApplicationListener<MyEvent>, ApplicationContextAware {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void onApplicationEvent(MyEvent event) {
-        logger.info("MyListener3 get event: {}", event.getSource());
+        if (applicationContext.getParent() != null) {
+            logger.info("MyListener3 get event: {}", event.getSource());
+        }
+    }
+
+    public ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
