cf5fe6885e3934c13b7307ce6725058a158169cf
xiaokexiang <xxiaokexiang@gmail.com>
2021-01-13 10:13:43 +0800

    [hadoop] env init

---
+++ spring-bigdata-dependencies/hadoop-client/src/main/java/io/spring/hadoop/HadoopClientApplication.java
@@ -0,0 +1,13 @@
+package io.spring.hadoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HadoopClientApplication.class, args);
+    }
+
+}
--- spring-bigdata-dependencies/hadoop-client/src/main/java/io/spring/hadoop/client/HdfsClient.java
+++ spring-bigdata-dependencies/hadoop-client/src/main/java/io/spring/hadoop/client/HdfsClient.java
@@ -1,31 +1,51 @@
 package io.spring.hadoop.client;
 
-import java.io.IOException;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.springframework.core.io.ClassPathResource;
+
+import java.net.URI;
 
 /**
  * @author xiaokexiang
- * @since 2021/1/12
+ * @since 2021/1/12 tips:
+ * 1. 用户权限问题: 使用FileSystem.get()需添加vm args: -DHADOOP_USER_NAME=root 或 使用FileSystem.get(conf, uri, username)
+ * 2. 提示缺少winUtils: https://github.com/steveloughran/winutils/tree/master/hadoop-2.8.1
+ * 3. 配置文件的优先级问题： configuration > client resource config > server config
  */
 @Slf4j
 public class HdfsClient {
 
-    /**
-     * 用户权限问题添加vm args: -DHADOOP_USER_NAME=root
-     */
+    private static final Configuration configuration = new Configuration();
+    private static final Path PATH = new Path("/client");
+    private FileSystem fileSystem;
+
+    static {
+        configuration.set("dfs.replication", "3");
+    }
+
+    public FileSystem getFileSystem() {
+        return this.fileSystem;
+    }
+
+    @SneakyThrows
+    public HdfsClient() {
+        this.fileSystem = FileSystem.get(URI.create("hdfs://hadoop-master:9000"), configuration, "root");
+    }
+
+    @SneakyThrows
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.set("fs.defaultFS", "hdfs://hadoop-master:9000");
-        try (FileSystem fileSystem = FileSystem.get(configuration)) {
-            if (!fileSystem.mkdirs(new Path("/client"))) {
-                log.info("mkdir failed");
-            }
-            fileSystem.delete(new Path("/client"), true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        HdfsClient client = new HdfsClient();
+        FileSystem fileSystem = client.getFileSystem();
+        // 1. 创建文件夹
+        fileSystem.mkdirs(PATH);
+        // 2. 上传文件（配置有优先级的概念），同名文件默认会覆盖
+        ClassPathResource resource = new ClassPathResource("static/local.txt");
+        fileSystem.copyFromLocalFile(new Path(resource.getURL().getPath()), PATH.suffix("/local.txt"));
+        // 3. 删除文件
+        fileSystem.delete(PATH.suffix("/local.txt"), false);
     }
 }
