874ce5f6b535894da941455a71afb34a3ace8cd5
xiaokexiang <xxiaokexiang@gmail.com>
2022-01-04 14:56:43 +0800

    MST-prim 100%

--- spring-boot-dependencies/spring-common/src/main/java/io/spring/common/algorithm/classic/Mst.java
+++ spring-boot-dependencies/spring-common/src/main/java/io/spring/common/algorithm/classic/Mst.java
@@ -39,7 +39,8 @@ import java.util.Arrays;
  * 5. 我们从F点出发，此时X[] ={A->C[7],B->D[9],F->D[4]},min(X)为F->D[4],将D加入Y,Y[]={A,G,B,E,F,D}
  * 6. 我们从D点出发，此时X[] ={A->C[7]},将C加入Y,Y[]={A,G,B,E,F,D,C}
  * <p>
- * 数据结构思路：
+ * <p>
+ * 数据结构思路-普利姆算法（1.先计算当前已选顶点到其他顶点的边的集合。 2.在边的集合中寻找最短的边，并将此边对应的点作为下次计算的顶点。3. 更新路径）：
  * minimum： 用于存放当前已选择的集合中到达对应各个村庄的边的最小值
  * parent： 用于对应minimum数组中此时边对应的父亲村庄是谁
  * 第一次遍历：
@@ -54,7 +55,12 @@ import java.util.Arrays;
  * name    a b c d e f g
  * mini   -1 3 7 0 4 6 -1
  * parent  - g a - g g -
- * 当前已选择集合（a,g节点）中，g->b为3（需要注意这里本来是a->b且边为5，被替换的原因是因为到达b点的村庄有a和g，a->b > g->b，所以被替换掉）
+ * 当前已选择集合（a,g节点）中，g->b为3（需要注意这里本来是a->b且边为5，被替换的原因是因为到达b点的村庄有a和g，a->b > g->b，所以被替换掉）,a->c为7，g->e为4，g->f为6
+ * 最小的边为g->b，将b点作为下一次遍历的村咋黄，同时将b点对应的mini设置为-1，表示b点已经被选择
+ * 如此循环遍历。
+ *
+ * <p>
+ * 数据结构思路-克鲁斯卡尔算法：
  */
 public class Mst {
     // 村庄集合
@@ -71,9 +77,10 @@ public class Mst {
     };
 
     /**
-     *
+     * 基于普利姆算法实现（选择当前顶点到其他点中边最小的那条路径）
+     * start用于表示起点在village中的索引位置
      */
-    public static void prim() {
+    public static void prim(int start) {
         int length = village.length;
         // 已选顶点集合到该顶点的所有边中权值最小的那条边的大小，如果值为-1表明当前index对应的村庄已经被选择了，不需要考虑边的大小
         int[] minimum = new int[length];
@@ -82,55 +89,44 @@ public class Mst {
         char[] parent = new char[length];
         // 记录返回值顺序
         String[] result = new String[length];
+        // 用于记录总长度
         int sum = 0;
-        for (int a = 0, b = 0; a < distance.length && b < distance.length; b++) {
+        // a表示每次最短边对应的点,b表示循环多少次，7个点最少只有7条边
+        for (int a = start, b = 0; a < distance.length && b < distance.length; b++) {
             // 特殊处理下起点村庄
             if (b == 0) {
-                result[b] = "0->" + village[b];
-                minimum[b] = -1;
+                result[b] = "0->" + village[a];
+                minimum[a] = Integer.MAX_VALUE;
                 continue;
             }
+            // 用于表示当前minimum中的最短边的位置
+            int index = 0;
             for (int c = 0; c < distance[a].length; c++) {
-                // 如果对应的村庄位置不为-1&当前最小路径表对应村庄的边大于当前路径表中的边，那么替换最小路径表中对应边的值
+                // 如果对应的村庄位置可达（!= -1）& 当前最小路径表对应村庄的边大于当前路径表中的边，那么替换最小路径表中对应边的值
                 if (-1 != distance[a][c] && minimum[c] > distance[a][c]) {
-                    // 1. update
+                    // 1. update 更新最小路径表 & 父节点表
                     minimum[c] = distance[a][c];
                     parent[c] = village[a];
                     // 如果已经记录了ac，那么将ca置为-1，下次不需要进行判断
                     distance[c][a] = -1;
                 }
+                // 2. scan 依次比较minimum的前一位与后一位，即寻找minimum中最小的权值边即 查询 已选顶点集合到该某顶点的所有边中 权值最小的那条边 的index
+                if (minimum[index] > minimum[c]) {
+                    index = c;
+                }
             }
-            // 2. scan： 寻找minimum中最小的权值边
-            int index = scan(minimum);
-            sum += minimum[index];
-            // 将minimum中最短边的村庄标记为已选择，即-1
-            minimum[index] = -1;
             // 3. add： 将最小权值的村庄加入集合中
             result[b] = parent[index] + "->" + village[index];
+            sum += minimum[index];
+            // 将minimum中最短边的村庄标记为已选择，即Integer.MAX_VALUE - 1
+            minimum[index] = Integer.MAX_VALUE;
             // 并从最小边的点开始继续循环执行
             a = index;
-
         }
-        System.out.println(Arrays.toString(result));
-        System.out.println(sum);
-    }
-
-    /**
-     * 查询 已选顶点集合到该某顶点的所有边中 权值最小的那条边 的index
-     */
-    public static int scan(int[] minimum) {
-        int min = Integer.MAX_VALUE;
-        int index = 0;
-        for (int i = 0; i < minimum.length; i++) {
-            if (minimum[i] != 0 && minimum[i] != -1 && min > minimum[i]) {
-                min = minimum[i];
-                index = i;
-            }
-        }
-        return index;
+        System.out.println(Arrays.toString(result) + ", 最短长度为: " + sum);
     }
 
     public static void main(String[] args) {
-        prim();
+        prim(2);
     }
 }
